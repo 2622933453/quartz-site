f3825a351aaad192717f371ba0f5f93bdf027853
2622933453 <2622933453@users.noreply.github.com>
2026-09-16 16:29:46 +0000

    deploy: 9bcb1e053a33167bae3abeb0e5afe7169706afcc

--- postscript-c96693d5.js
+++ postscript-c96693d5.js
@@ -2,7 +2,7 @@ await Promise.all([
   import("./static/scripts/script-0-f7c52b6a.js"),
   import("./static/scripts/script-1-0e41d61b.js"),
   import("./static/scripts/script-2-5b00c083.js"),
-  import("./static/scripts/script-3-1f9394e6.js"),
+  import("./static/scripts/script-3-4e3473f6.js"),
   import("./static/scripts/script-4-cc8afa9d.js"),
   import("./static/scripts/script-5-7e107d7e.js"),
   import("./static/scripts/script-6-5ef560a8.js"),
